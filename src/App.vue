fca37746c53d91120055ab2f179b2cea6db3d4e5
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-01-23 15:39:44 -0300

    chore: create header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app">
-    <h1>Hello world</h1>
+  <div class="app">
+    <header class="app__header">
+      <h1>Screen Record</h1>
+    </header>
   </div>
 </template>
 
@@ -11,5 +13,18 @@
 </script>
 
 <style lang="scss" scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+  background: #1A1A40;
 
+  &__header {
+    background: #270082;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem 0;
+    color: #e0dfdf;
+  }
+}
 </style>
